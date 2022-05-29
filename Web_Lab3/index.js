1ce6e7ee7692f6ef05d1dca1f9c14e5660380cbc
andryshkaadr <101631355+andryshkaadr@users.noreply.github.com>
2022-05-29 19:54:27 +0300

    Add files via upload

---
+++ Web_Lab3/index.js
@@ -0,0 +1,7 @@
+document.querySelector('.btn').addEventListener('click', function () {
+    document.querySelector('.content').classList.add('visibility');
+});
+
+ document.querySelector('.overlap').addEventListener('click', function () {
+    document.querySelector('.content').classList.remove('visibility');
+});
